00525a8776a251019b536bc0b5ceccb17babd5c2
Drystan-Furor <80721880+Drystan-Furor@users.noreply.github.com>
2023-10-01 16:43:04 +0200

    body

--- build/js/npc/generators/body.js
+++ build/js/npc/generators/body.js
@@ -1,4 +1,5 @@
 import {classMapping, setClassMapping} from "../races/factory/classMapping";
+import {ucFirst} from "../races/factory/utility";
 
 export class Body {
     constructor(dndRace, npc) {
@@ -45,15 +46,42 @@ export class Body {
         return bodySizes[Math.floor(Math.random() * bodySizes.length)];
     }
 
-    _bodyBuilder(new_npc) {
-        const manWoman = new_npc.getManWoman();
-        const heshe = new_npc.getHeShe();
-        const hisher = new_npc.getHisHer();
-        const gender = new_npc.getGender();
-
-        this.body = `Built ${this.bodyType}, ${heshe} has a ${gender} body with ${this.bodyShape}`;
+    _bodyBuilder(genderNouns) {
+        this.body = this._bodyString(genderNouns);
         return this.body;
     }
+
+    _bodyString(genderNouns) {
+        const manWoman = genderNouns.getManWoman();
+        const heshe = genderNouns.getHeShe();
+        const hisher = genderNouns.getHisHer();
+        const gender = genderNouns.getGender();
+        const string = [
+            `Built ${this.bodyType}, ${heshe} has a ${this.bodySize} ${gender} body with ${this.bodyShape}`,
+            `${ucFirst(heshe)} possesses a ${gender} body, ${this.bodyType} in build, characterized by a ${this.bodySize} ${this.bodyShape}`,
+            `With a ${this.bodySize} ${gender} body, ${heshe} is ${this.bodyType} and exhibits ${this.bodyShape}`,
+            `The ${gender} form of this ${this.bodySize} ${manWoman} is ${this.bodyType}, marked by ${this.bodyShape}`,
+            `${ucFirst(heshe)} carries ${hisher} ${this.bodySize} ${gender} physique, ${this.bodyType} with ${this.bodyShape}`,
+            `In terms of physique, ${heshe} is ${this.bodyType}, having a ${gender} body with ${this.bodySize} ${this.bodyShape}`,
+            `The ${gender} structure of this ${this.bodySize} ${manWoman} is ${this.bodyType}, defined by ${this.bodyShape}`,
+            `This ${this.bodySize} ${manWoman} has a ${gender} build, ${this.bodyType} and outlined by ${this.bodyShape}`,
+            `Exhibiting a ${this.bodySize} ${gender} body, ${heshe} is structured ${this.bodyType} with ${this.bodyShape}`,
+            `${ucFirst(heshe)} has a ${this.bodySize} ${gender} frame, built ${this.bodyType} and features ${this.bodyShape}`,
+            `${ucFirst(hisher)} ${gender} body is ${this.bodySize} ${this.bodyType} and showcases ${this.bodyShape}`,
+            `This ${manWoman}'s ${this.bodySize} ${gender} physique is ${this.bodyType}, highlighted by ${this.bodyShape}`,
+            `With a ${this.bodySize} ${this.bodyType} build, ${heshe} presents a ${gender} body featuring ${this.bodyShape}`,
+            `${ucFirst(heshe)}'s ${gender} form is ${this.bodyType}, distinguished by ${this.bodySize} ${this.bodyShape}`,
+            `This ${this.bodySize} ${manWoman} exhibits a ${gender} body, ${this.bodyType} in structure, and is characterized by ${this.bodyShape}`,
+            `The ${this.bodySize} ${gender} body of this ${manWoman} is ${this.bodyType} and is marked by ${this.bodyShape}`,
+            `${ucFirst(hisher)} ${this.bodySize} physique is ${this.bodyType}, a representation of ${gender} body, detailed with ${this.bodyShape}`,
+            `In ${this.bodyType} form, ${heshe} displays a ${this.bodySize} ${gender} body, accentuated by ${this.bodyShape}`,
+            `This ${manWoman} holds a ${gender} body, ${this.bodyType} in build, and is defined by ${this.bodySize} ${this.bodyShape}`,
+            `${ucFirst(hisher)} ${gender} structure is ${this.bodySize}, ${this.bodyType} and is adorned with ${this.bodyShape}`,
+        ];
+        return string[Math.floor(Math.random() * string.length)];
+    }
+
+
 
     getBodyType() {
         return this.bodyType;
