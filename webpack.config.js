9b742bf75e45044fbee84ccbf15c90571a2f72c6
Drystan-Furor <80721880+Drystan-Furor@users.noreply.github.com>
2023-10-18 07:21:49 +0200

    weather Generator html

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const webpack = require('webpack');
 
 
 module.exports = {
-  mode: 'production', // Set to 'production' or 'none' as needed
+  mode: 'development', // Set to 'production' or 'none' as needed
   entry: {
     main: './build/js/main.js',
     dndnpcrng: './build/js/dndnpcrng.js',
