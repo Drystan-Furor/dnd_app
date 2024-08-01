b4a0f44aa414dd94c8d2e1ffe9b52ce1b0d4c613
Drystan-Furor <80721880+Drystan-Furor@users.noreply.github.com>
2024-08-01 13:03:56 +0200

    random number dungeons

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const webpack = require('webpack');
 
 
 module.exports = {
-  mode: 'development', // Set to 'production' or 'none' as needed //development
+  mode: 'production', // Set to 'production' or 'none' as needed //development
   entry: {
     main: './build/js/main.js',
     dndnpcrng: './build/js/dndnpcrng.js',
